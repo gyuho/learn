5e56597293030bad9341a9efeade925044b355b2
Gyu-Ho Lee <gyuhox@gmail.com>
2015-09-22 20:35:04 -0700

    update concurrency code

--- doc/go_concurrency/code/26_range_channel_close.go
+++ doc/go_concurrency/code/26_range_channel_close.go
@@ -3,30 +3,53 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int)
-
-	for i := 0; i < 5; i++ {
-		go func(i int) {
-			ch <- i
-		}(i)
-	}
-
-	cn := 0
-	for v := range ch {
-		fmt.Println(v)
-		cn++
-		if cn == 5 {
-			close(ch)
+	func() {
+		ch := make(chan int)
+		for i := 0; i < 5; i++ {
+			go func(i int) {
+				ch <- i
+			}(i)
 		}
-	}
-	// 0
-	// 1
-	// 2
-	// 3
-	// 4
-
-	v, ok := <-ch
-	fmt.Println(v, ok) // 0 false
-	// any value received from closed channel succeeds without blocking
-	// , returning the zero value of channel type and false.
+		cn := 0
+		for v := range ch {
+			fmt.Println(v)
+			cn++
+			if cn == 5 {
+				close(ch)
+			}
+		}
+		// 0
+		// 1
+		// 2
+		// 3
+		// 4
+		v, ok := <-ch
+		fmt.Println(v, ok) // 0 false
+		// any value received from closed channel succeeds without blocking
+		// , returning the zero value of channel type and false.
+	}()
+	func() {
+		ch := make(chan struct{})
+		slice := []string{"A", "B", "C", "D", "E"}
+		for _, v := range slice {
+			go func(v string) {
+				fmt.Println("Printing:", v)
+				ch <- struct{}{}
+			}(v)
+		}
+		cn := 0
+		for range ch {
+			cn++
+			if cn == len(slice) {
+				close(ch)
+			}
+		}
+		/*
+			Printing: E
+			Printing: A
+			Printing: B
+			Printing: C
+			Printing: D
+		*/
+	}()
 }
