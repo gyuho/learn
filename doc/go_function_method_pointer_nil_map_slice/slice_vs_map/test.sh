06ad161227974e1ae51399a80cb815bff9323194
Gyu-Ho Lee <gyuhox@gmail.com>
2015-09-24 03:52:31 -0700

    Update test.sh

--- doc/go_function_method_pointer_nil_map_slice/slice_vs_map/test.sh
+++ doc/go_function_method_pointer_nil_map_slice/slice_vs_map/test.sh
@@ -10,8 +10,8 @@ go test -v -race ./...;
 
 printf "\n"
 echo "TEST #3 $ go test -bench . -benchmem -cpu 1,2,4,8,16"
-go test -opt="Slice" -bench . -benchmem -cpu 1,2,4,8,16 > slice.txt;
-go test -opt="Map"   -bench . -benchmem -cpu 1,2,4,8,16 > map.txt;
+go test -opt="Slice" -bench . -benchmem -cpu 1,2,4,8 > slice.txt;
+go test -opt="Map"   -bench . -benchmem -cpu 1,2,4,8 > map.txt;
 
 go get -v -u golang.org/x/tools/cmd/benchcmp;
 benchcmp slice.txt map.txt > benchmark_results.txt;
