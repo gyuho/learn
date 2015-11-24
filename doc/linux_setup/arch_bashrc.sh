48291816ca0a5b0edbc228d8af69430b7224426f
Gyu-Ho Lee <gyuhox@gmail.com>
2015-11-24 11:08:16 -0800

    use python2 instead

--- doc/linux_setup/arch_bashrc.sh
+++ doc/linux_setup/arch_bashrc.sh
@@ -115,4 +115,6 @@ alias cee='cd $HOME/go/src/github.com/coreos;'
 alias cgg='cd $HOME/go/src/github.com/gyuho;'
 alias learn='cd $HOME/go/src/github.com/gyuho/learn/doc;'
 
+alias python='python2'
+
 
