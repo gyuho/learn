306a7bbed7663789ca74284d658418016281e019
Gyu-Ho Lee <gyuhox@gmail.com>
2015-11-26 22:41:14 -0800

    delete pcmanfm in arch.sh

--- doc/linux_setup/arch.sh
+++ doc/linux_setup/arch.sh
@@ -45,7 +45,6 @@ sudo pacman --noconfirm -Su openssh;
 sudo pacman --noconfirm -Su terminator xterm;
 sudo pacman --noconfirm -Su networkmanager;
 sudo pacman --noconfirm -Su net-tools;
-sudo pacman --noconfirm -Su pcmanfm;
 sudo pacman --noconfirm -Su libreoffice-fresh hunspell hunspell-en;
 sudo pacman --noconfirm -Su transmission-daemon;
 sudo pacman --noconfirm -Su transmission-qt;
