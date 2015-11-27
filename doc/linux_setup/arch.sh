ba26ca9764f2dc311e45fb3139adc6d798e15042
Gyu-Ho Lee <gyuhox@gmail.com>
2015-11-27 15:16:39 -0800

    update arch.sh

--- doc/linux_setup/arch.sh
+++ doc/linux_setup/arch.sh
@@ -48,6 +48,7 @@ sudo pacman --noconfirm -Su net-tools;
 sudo pacman --noconfirm -Su libreoffice-fresh hunspell hunspell-en;
 sudo pacman --noconfirm -Su transmission-daemon;
 sudo pacman --noconfirm -Su transmission-qt;
+sudo pacman --noconfirm -Su vlc;
 
 sudo pacman --noconfirm -Su noto-fonts noto-fonts-cjk noto-fonts-emoji;
 sudo pacman --noconfirm -Su libhangul ttf-baekmuk ttf-dejavu ttf-droid;
