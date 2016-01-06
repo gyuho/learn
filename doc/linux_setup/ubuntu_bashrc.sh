a7f6c102c4564364a28ed937ab1be16a8e57ec5e
Gyu-Ho Lee <gyuhox@gmail.com>
2016-01-06 09:21:28 -0800

    concatenate strings in bash

--- doc/linux_setup/ubuntu_bashrc.sh
+++ doc/linux_setup/ubuntu_bashrc.sh
@@ -123,8 +123,11 @@ git remote -v
 
 alias work='
 cd $GOPATH/src/github.com/coreos/etcd && \
+title="etcd" && \
 current_branch=$(git branch | sed -n -e "s/^\* \(.*\)/\1/p") && \
-temp_dir="etcd_$current_branch_$(date +"%Y%m%d%H%M%S")" && \
+date_string=$(date +"%Y%m%d%H%M%S") && \
+underscore="_" && \
+temp_dir=$title$underscore$current_branch$underscore$date_string && \
 cd $GOPATH/src/github.com/coreos && \
 mv etcd $temp_dir && \
 rm -rf etcd && \
