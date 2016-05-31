22088dea1a2ec0d4170ccfd881ee8bf5829adcd4
Gyu-Ho Lee <gyuhox@gmail.com>
2016-05-31 15:56:16 -0700

    go sort: search with BST

--- doc/go_sort_algorithm/code/13_sort_search_bst.go
+++ doc/go_sort_algorithm/code/13_sort_search_bst.go
@@ -9,4 +9,14 @@ func main() {
 	names := []string{"a", "b", "c", "hello", "d", "e"}
 	idx := sort.Search(len(names), func(i int) bool { return names[i] == "hello" })
 	fmt.Println(idx, names[idx]) // 3 hello
+
+	idx = sort.Search(len(names), func(i int) bool { return names[i] == "x" })
+	if idx == len(names) {
+		fmt.Println("x is not found")
+	}
 }
+
+/*
+3 hello
+x is not found
+*/
