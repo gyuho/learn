e40d4e61181b749ef7f02f47966d55eb1bada4cf
Gyu-Ho Lee <gyuhox@gmail.com>
2016-06-05 20:04:45 -0700

    go sort: add unsorted

--- doc/go_sort_algorithm/code/14_sort_search_unsorted.go
+++ doc/go_sort_algorithm/code/14_sort_search_unsorted.go
@@ -7,6 +7,26 @@ import (
 
 func main() {
 	{
+		ns := []int{1, 2, 0, -1, -2}
+		num := -1
+		// Binary search the smallest index at which the number is greater than the given one (== 10)
+		fmt.Println("IsSorted:", sort.IsSorted(sort.IntSlice(ns)))
+		idx := sort.Search(len(ns), func(i int) bool {
+			fmt.Println("searching", ns[i])
+			return ns[i] >= num
+		})
+		fmt.Println("Result:", ns[idx])
+	}
+	/*
+	   IsSorted: false
+	   searching 0
+	   searching 2
+	   searching 1
+	   Result: 1
+	*/
+
+	println()
+	{
 		ns := []int{2, 100, 1, 20, 10, 50}
 		num := 5
 		// Binary search the smallest index at which the number is greater than the given one (== 10)
@@ -26,7 +46,6 @@ func main() {
 	*/
 
 	println()
-
 	{
 		ns := []int{2, 100, 1, 20, 10, 50}
 		sort.Ints(ns)
