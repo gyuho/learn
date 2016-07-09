9ad8c7dd334cbfdfa0e03e4627c4f6f1ef3f5f6c
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-09 00:31:24 -0700

    go archive: update untar example

--- doc/go_archive_compress_encoding_decoding/code/07_download_untar.go
+++ doc/go_archive_compress_encoding_decoding/code/07_download_untar.go
@@ -2,44 +2,80 @@ package main
 
 import (
 	"archive/tar"
+	"archive/zip"
 	"bytes"
 	"compress/gzip"
 	"fmt"
 	"html/template"
 	"io"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func main() {
-	const (
-		fileNameTmpl     = "etcd-{{.Version}}-linux-amd64.tar.gz"
-		extractedDirTmpl = "etcd-{{.Version}}-linux-amd64"
-		googleTmpl       = "https://storage.googleapis.com/etcd/{{.Version}}/etcd-{{.Version}}-linux-amd64.tar.gz"
-		githubTmpl       = "https://github.com/coreos/etcd/releases/download/{{.Version}}/etcd-{{.Version}}-linux-amd64.tar.gz"
-	)
-	ver := "v3.0.2"
-	fileName := insertVersion(fileNameTmpl, ver)
-	extractedDir := insertVersion(extractedDirTmpl, ver)
+	dir, err := Download(os.TempDir(), "v3.0.2", "linux")
+	fmt.Println("Downloaded at", dir)
+	fmt.Println(err)
 
-	os.RemoveAll(fileName)
-	os.RemoveAll(extractedDir)
-
-	if err := downloadExtractEtcd(fileName, insertVersion(googleTmpl, ver), insertVersion(githubTmpl, ver)); err != nil {
-		log.Fatal(err)
-	}
-
-	os.RemoveAll(fileName)
-	os.RemoveAll(extractedDir)
+	os.RemoveAll(dir)
 }
 
-func downloadExtractEtcd(fileName string, urls ...string) error {
+const (
+	fileNamelLinux    = "etcd-{{.Version}}-linux-amd64.tar.gz"
+	extractedDirLinux = "etcd-{{.Version}}-linux-amd64"
+
+	fileNameDarwin     = "etcd-{{.Version}}-darwin-amd64.zip"
+	extractedDirDarwin = "etcd-{{.Version}}-darwin-amd64"
+
+	fileNameWindows     = "etcd-{{.Version}}-windows-amd64.zip"
+	extractedDirWindows = "etcd-{{.Version}}-windows-amd64"
+
+	googleTmpl = "https://storage.googleapis.com/etcd/{{.Version}}/{{.FileName}}"
+	githubTmpl = "https://github.com/coreos/etcd/releases/download/{{.Version}}/{{.FileName}}"
+)
+
+// Download downloads and extracts etcd release binaries.
+//
+//   dir, _ = Download(os.TempDir(), "v3.0.2", "linux")
+//   dir, _ = Download(os.TempDir(), "v3.0.2", "darwin")
+//   dir, _ = Download(os.TempDir(), "v3.0.2", "windows")
+//
+// downloads the etcd v3.0.2 and extracts to 'dir' directory.
+func Download(targetDir, ver, hostOS string) (string, error) {
+	var (
+		fileNameTmpl     string
+		extractedDirTmpl string
+		extractFunc      func(string, string) error
+	)
+	switch hostOS {
+	case "linux":
+		fileNameTmpl, extractedDirTmpl = fileNamelLinux, extractedDirLinux
+		extractFunc = extractTarGz
+	case "osx", "darwin":
+		fileNameTmpl, extractedDirTmpl = fileNameDarwin, extractedDirDarwin
+		extractFunc = extractZip
+	case "windows":
+		fileNameTmpl, extractedDirTmpl = fileNameWindows, extractedDirWindows
+		extractFunc = extractZip
+	default:
+		return "", fmt.Errorf("unknown OS %q", hostOS)
+	}
+
+	var (
+		fileName     = insertVersionFileName(fileNameTmpl, ver, "")
+		extractedDir = insertVersionFileName(extractedDirTmpl, ver, "")
+		urls         = []string{insertVersionFileName(googleTmpl, ver, fileName), insertVersionFileName(githubTmpl, ver, fileName)}
+	)
+	fileName = filepath.Join(targetDir, fileName)
+	extractedDir = filepath.Join(targetDir, extractedDir)
+
 	f, err := os.Create(fileName)
 	if err != nil {
-		return err
+		return extractedDir, err
 	}
 	defer f.Close()
 
@@ -49,30 +85,27 @@ func downloadExtractEtcd(fileName string, urls ...string) error {
 		if err != nil {
 			continue
 		}
-		defer resp.Body.Close()
+		defer gracefulClose(resp)
 
-		_, err = io.Copy(f, resp.Body)
-		if err != nil {
+		if _, err = io.Copy(f, resp.Body); err != nil {
 			continue
 		}
 		break
 	}
 	if err != nil {
-		return err
+		return extractedDir, err
 	}
-	return extractTarGz(fileName)
+
+	return extractedDir, extractFunc(fileName, targetDir)
 }
 
-func insertVersion(tmpl, ver string) string {
-	buf := new(bytes.Buffer)
-	if err := template.Must(template.New("tmpl").Parse(tmpl)).Execute(buf, struct{ Version string }{ver}); err != nil {
-		panic(err)
-	}
-	return buf.String()
+func gracefulClose(resp *http.Response) {
+	io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
 }
 
-func extractTarGz(target string) error {
-	f, err := os.Open(target)
+func extractTarGz(fileToExtract, dir string) error {
+	f, err := os.Open(fileToExtract)
 	if err != nil {
 		return err
 	}
@@ -93,24 +126,54 @@ func extractTarGz(target string) error {
 			return err
 		}
 
-		if err := untar(tr, header); err != nil {
+		if err := untar(tr, header, dir); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func untar(tr *tar.Reader, header *tar.Header) error {
+func extractZip(fileToExtract, dir string) error {
+	zr, err := zip.OpenReader(fileToExtract)
+	if err != nil {
+		return err
+	}
+	defer zr.Close()
+
+	for _, zf := range zr.File {
+		if err := unzip(zf, dir); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func untar(tr *tar.Reader, header *tar.Header, dir string) error {
 	switch header.Typeflag {
 	case tar.TypeDir:
-		return os.MkdirAll(header.Name, 0700)
+		return os.MkdirAll(filepath.Join(dir, header.Name), 0700)
 	case tar.TypeReg, tar.TypeRegA:
-		return writeFile(header.Name, tr, header.FileInfo().Mode())
+		return writeFile(filepath.Join(dir, header.Name), tr, header.FileInfo().Mode())
 	case tar.TypeSymlink:
-		return writeSymlink(header.Name, header.Linkname)
+		return writeSymlink(filepath.Join(dir, header.Name), header.Linkname)
 	default:
 		return fmt.Errorf("%s has unknown type %v", header.Name, header.Typeflag)
 	}
+}
+
+func unzip(zf *zip.File, dir string) error {
+	if strings.HasSuffix(zf.Name, "/") {
+		return os.MkdirAll(filepath.Join(dir, zf.Name), 0700)
+	}
+
+	rc, err := zf.Open()
+	if err != nil {
+		return fmt.Errorf("%s: open compressed file: %v", zf.Name, err)
+	}
+	defer rc.Close()
+
+	return writeFile(filepath.Join(dir, zf.Name), rc, zf.FileInfo().Mode())
 }
 
 func writeFile(fpath string, rd io.Reader, mode os.FileMode) error {
@@ -137,4 +200,12 @@ func writeSymlink(fpath string, target string) error {
 		return err
 	}
 	return os.Symlink(target, fpath)
+}
+
+func insertVersionFileName(tmpl, ver, fileName string) string {
+	buf := new(bytes.Buffer)
+	if err := template.Must(template.New("tmpl").Parse(tmpl)).Execute(buf, struct{ Version, FileName string }{ver, fileName}); err != nil {
+		panic(err)
+	}
+	return buf.String()
 }
