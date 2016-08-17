faa8d743378cfbc776e2d6664e941b87a409216c
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-17 12:11:50 -0700

    typescript: add hello world with class

---
+++ doc/typescript/01-class/greeter.js
@@ -0,0 +1,11 @@
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+document.getElementsByClassName('greeter')[0].innerHTML = greeter.greet();
---
+++ doc/typescript/01-class/greeter.ts
@@ -0,0 +1,13 @@
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+
+document.getElementsByClassName('greeter')[0].innerHTML = greeter.greet();
